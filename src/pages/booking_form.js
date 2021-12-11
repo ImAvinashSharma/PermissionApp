2aba495421eb09097ec3007966fa617c2ce0e34e
avi9360 <savinash406@gmail.com>
2021-12-11 12:22:52 +0530

    performance improved

--- src/pages/booking_form.js
+++ src/pages/booking_form.js
@@ -5,15 +5,15 @@ import "../movie_details.css";
 export const Bookingform = () => {
   const history = useHistory();
   const location = useLocation();
-  const { profile, password, username, mobile, email } = location.state.profile;
-
+  const { profile, password, name, mobile, email } = location.state;
+  console.log(location.state);
   const [bookingdate, setbookingdate] = useState("");
   const selectCeats = e => {
     e.preventDefault();
-    if (username === "" || bookingdate === "") {
+    if (name === "" || bookingdate === "") {
       alert("please select booking date");
     } else {
-      history.push({ pathname: "/pickceat", state: { profile: profile, email: email, username: username, mobile: mobile, bookingdate: bookingdate, password: password } });
+      history.push({ pathname: "/pickceat", state: { profile: profile, email: email, username: name, mobile: mobile, bookingdate: bookingdate, password: password } });
     }
   };
   return (
@@ -21,7 +21,7 @@ export const Bookingform = () => {
       <form style={{ background: "#f6f5f7" }}>
         <h1>Permission request</h1>
         <br />
-        <input type="text" placeholder="Username" value={username} />
+        <input type="text" placeholder="Name" value={name} />
         <input type="date" placeholder="Pick Date" max={null} min={null} value={bookingdate} onChange={e => setbookingdate(e.target.value)} />
         <br />
         <button onClick={selectCeats}>Select Slots</button>
